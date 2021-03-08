bcd3d8f700121b78dabd5906d25c1d02161cb031
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-03-08 15:23:55 +0300

    Fix register_button_login_label translation

--- src/i18n/en-us/index.ts
+++ src/i18n/en-us/index.ts
@@ -4,7 +4,7 @@ export default {
   login_input_password_label: "Password",
   login_button_submit_label: "Log In",
   login_button_register_label: "Register",
-
+  
   register_title: "Welcome!",
   register_input_username_label: "Username",
   register_input_email_label: "E-mail",
@@ -12,9 +12,8 @@ export default {
   register_input_password_label: "Password",
   register_input_password_repeat_label: "Repeat password",
   register_button_submit_label: "Register",
-  login_button_login_label: "Login",
-
-
+  register_button_login_label: "Login",
+  
   register_invalid_char_in_username_error: "Username must contain only letters, numbers and hyphens/underscores",
   register_too_short_username_error: "Username must me longer than 1 symbol",
   register_too_long_username_error: "Username must be shorter than 30 symbols",
@@ -24,27 +23,27 @@ export default {
   register_username_taken_error: "Username is already taken",
   register_email_taken_error: "E-mail is already taken",
   register_request_error: "Request error",
-
+  
   register_too_short_password_error: "Too short password",
   register_password_must_contain_uppercase_error: "Password must contain uppercase letters",
   register_password_must_contain_lowercase_error: "Password must contain lowercase letters",
   register_password_must_contain_numbers_error: "Password must contain numbers",
   register_different_passwords_error: "Passwords are not equal",
-
+  
   main_login_button_label: "Log In",
   main_register_button_label: "Register",
-
+  
   main_desktop_logout_label: "Log Out",
   main_desktop_settings_label: "Settings",
   main_desktop_auctions_label: "Auctions",
-
+  
   main_header_feed_label: "Feed",
   main_header_auctions_label: "Auctions",
   main_header_new_post_label: "New post",
   main_header_new_auction_label: "New auction",
   main_header_upload_image_label: "Upload image",
-
-
+  
+  
   me_button_commissions_label: "Commissions",
   me_button_albums_label: "Albums",
   me_button_chat_label: "Chat",
