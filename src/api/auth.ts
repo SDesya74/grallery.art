2c7860f7edd2225265e18ac0f22152eff2dc0247
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-03-08 15:09:48 +0300

    Make authorize method throw errors

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,35 +6,33 @@ import { api } from "src/api/index"
 export async function refresh(): Promise<SessionModel> {
   return new Promise(async (resolve, reject) => {
     if (!RefreshToken.exists()) return reject()
-
+    
     const { ok, payload } = await api.post<SessionModel>("refresh", {
       refresh_token: RefreshToken.get()
     })
-
+    
     if (ok) resolve(payload)
     else reject(payload)
   })
 }
 
 
-export async function authorize(login: string, password: string): Promise<boolean> {
+export async function authorize(login: string, password: string): Promise<void> {
   const { ok, payload } = await api.post<SessionModel>("login", {
     login: login,
     password: password
   })
-
+  
   if (!ok) {
-    authorized = false
-    return false
+    closeSession()
+    throw new Error(payload.message)
   }
-
   openSession(payload)
-  return true
 }
 
 export function openSession(session: SessionModel): void {
   const { access, refresh } = session
-
+  
   AccessToken.set(access.token)
   RefreshToken.set(refresh.token)
   authorized = true
@@ -58,6 +56,6 @@ export async function isAuthorized() {
       closeSession()
     }
   }
-
+  
   return authorized && RefreshToken.exists()
 }
