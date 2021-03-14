3eefecd970739ff37a384df44259bff91cd77bf9
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-03-14 22:04:43 +0300

    Add routeTo method

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,14 +10,26 @@ export default route(function (/* { store, ssrContext } */) {
       : process.env.VUE_ROUTER_MODE === "history"
       ? createWebHistory
       : createWebHashHistory
-
+  
   router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
     history: createHistory(process.env.MODE === "ssr" ? void 0 : process.env.VUE_ROUTER_BASE)
   })
-
+  
   return router
 })
+
+export async function routeTo(name: string, params: { [key: string]: any } = {}) {
+  const { currentRoute } = router
+  await router.isReady()
+  await router.push({
+    name,
+    params: {
+      ...params,
+      ...currentRoute.value.params
+    }
+  })
+}
 
 export { router }
