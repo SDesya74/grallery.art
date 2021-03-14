dc5dad0b0581940522f581e47c2dfe1511f5733b
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-03-14 22:12:04 +0300

    Typo

--- src/pages/errors/404.vue
+++ src/pages/errors/404.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 .fullscreen.bg-primary.text-white.text-center.q-pa-md.flex.flex-center
-    div
-        div(style="font-size: 30vh") 404
-        .text-h2(style="opacity: 0.4") Oops. Nothing here...
-        q-btn.q-mt-xl(color="white" no-caps text-color="primary" unelevated :to="{ name: 'index' }") Go Home
+  div
+    div(style="font-size: 30vh") 404
+    .text-h2(style="opacity: 0.4") Oops. Nothing here...
+    q-btn.q-mt-xl(color="white" no-caps text-color="primary" unelevated :to="{ name: 'index' }") Go Home
 </template>
 
 <script lang="ts">
-    export default {}
+  export default {}
 </script>
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,31 +3,31 @@ import { LocalStorage, SessionStorage } from "quasar"
 type IStorage = LocalStorage | SessionStorage
 
 class Token {
-    constructor(private name: string, private storage: IStorage) {
+  constructor(private name: string, private storage: IStorage) {
+  }
+  
+  exists(): boolean {
+    return this.storage.has(this.name)
+  }
+  
+  set(token: string): void {
+    this.storage.set(this.name, token)
+  }
+  
+  get(): string {
+    if (!this.exists()) throw `${ this.name } is not exists`
+    return this.storage.getItem<string>(this.name)!
+  }
+  
+  remove(): void {
+    this.storage.remove(this.name)
+  }
+  
+  bearer() {
+    return {
+      "Authorization": `Bearer ${ this.get() }`
     }
-    
-    exists(): boolean {
-        return this.storage.has(this.name)
-    }
-    
-    set(token: string/*, expires: number*/): void {
-        this.storage.set(this.name, token)
-    }
-    
-    get(): string {
-        if (!this.exists()) throw `${ this.name } is not exists`
-        return this.storage.getItem<string>(this.name)!
-    }
-    
-    remove(): void {
-        this.storage.remove(this.name)
-    }
-    
-    bearer() {
-        return {
-            "Authorization": `Bearer ${ this.get() }`
-        }
-    }
+  }
 }
 
 export const AccessToken = new Token("access_token", SessionStorage)
