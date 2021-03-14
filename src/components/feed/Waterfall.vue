e0065d5498b67ff585c16dd72b6e8e1e90e57efa
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-03-14 21:59:29 +0300

    Make waterfall loading smoother

--- src/components/feed/Waterfall.vue
+++ src/components/feed/Waterfall.vue
@@ -1,75 +1,79 @@
 <template lang="pug">
-q-infinite-scroll(:offset="Math.floor($q.screen.height / 4)", :style="{ width }" @load="onLoad")
+div(:style="{ width }")
   .row.justify-center(ref="waterfallRow")
     .column(:style="{ width: columnWidth }" v-for="{ elements } in columns")
       slot(:element="element" name="element" v-for="element in elements")
-  template(v-slot:loading)
-    div(class="row justify-center q-my-md")
-      q-spinner-dots(color="primary" size="40px")
+  .absolute-bottom(v-intersection="onIntersection" style="z-index: -999; height: 25vh; width: inherit")
 </template>
 
 <script lang="ts">
-    import { computed, onMounted, ref, SetupContext } from "vue"
+  import { computed, onMounted, ref, SetupContext } from "vue"
+  
+  type Column = {
+    elements: any[],
+    height: number
+  }
+  
+  export default {
+    emits: [ "load" ],
+    props: {
+      width: {
+        type: String,
+        default: "auto"
+      },
+      columnAmount: {
+        type: [ Number, String ],
+        required: true
+      },
+      columnWidth: {
+        type: String,
+        required: true
+      },
+      elements: {
+        type: Array,
+        required: true
+      }
+    },
     
-    type Column = {
-        elements: any[],
-        height: number
-    }
-    
-    export default {
-        emits: [ "load" ],
-        props: {
-            width: {
-                type: String,
-                default: "auto"
-            },
-            columnAmount: {
-                type: [ Number, String ],
-                required: true
-            },
-            columnWidth: {
-                type: String,
-                required: true
-            },
-            elements: {
-                type: Array,
-                required: true
-            }
-        },
+    setup({ elements, columnAmount }: any, { emit }: SetupContext) {
+      const columns = ref<Column[]>([])
+      
+      function updateItems() {
+        const len = parseInt(columnAmount)
+        const newColumns: Array<Column> = Array<Column>(len)
+        for (let i = 0; i < len; i++) newColumns[i] = { elements: [], height: 0 }
         
-        setup({ elements, columnAmount }: any, { emit }: SetupContext) {
-            const columns = ref<Column[]>([])
-            
-            function updateItems() {
-                columns.value = Array<Column>(parseInt(columnAmount))
-                for (let i = 0; i < columns.value.length; i++) {
-                    columns.value[i] = { elements: [], height: 0 }
-                }
-                
-                for (const element of elements) {
-                    const min = Math.min(...columns.value.map(e => e.height))
-                    const index = columns.value.findIndex(e => e.height == min)
-                    const column = columns.value[index]
-                    if (!column) continue
-                    column.elements.push(element)
-                    column.height += element.height
-                }
-            }
-            
-            function onLoad(index: number, done: () => void) {
-                emit("load", () => {
-                    updateItems()
-                    done()
-                })
-            }
-            
-            onMounted(updateItems)
-            
-            const waterfallRow = ref()
-            const totalWidth = computed(() => waterfallRow.value.clientWidth)
-            
-            
-            return { onLoad, columns, waterfallRow, totalWidth }
+        for (const element of elements) {
+          const min = Math.min(...newColumns.map(e => e.height))
+          const index = newColumns.findIndex(e => e.height == min)
+          const column = newColumns[index]
+          
+          column.elements.push(element)
+          column.height += element.height
         }
+        
+        columns.value = newColumns
+      }
+      
+      function emitLoad() {
+        emit("load", updateItems)
+      }
+      
+      onMounted(emitLoad)
+      
+      const waterfallRow = ref()
+      const totalWidth = computed(() => waterfallRow.value.clientWidth)
+      const slot = ref()
+      
+      return {
+        columns, waterfallRow, totalWidth, slot,
+        
+        onIntersection(entry: any) {
+          if (!entry.isIntersecting) return
+          console.log("load data")
+          emitLoad()
+        }
+      }
     }
+  }
 </script>
