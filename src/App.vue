924ab0cb36f51056f22dae452694edf6a71b71a4
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-03-31 17:52:26 +0300

    Add default locale setting

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 router-view
 </template>
 
-<script lang="ts">
-    export default {}
+<script lang="ts" setup>
+  import { useI18n } from "vue-i18n"
+  import { useQuasar } from "quasar"
+
+  const { locale } = useI18n({ useScope: "global" })
+  locale.value = useQuasar().lang.getLocale() as string
 </script>
